e6ee8d855baed8349cfc9c5077777deb7f8717e6
Amit Upadhyay <amitu2016@gmail.com>
2023-11-13 17:59:24 +0530

    children tags in react

--- App.js
+++ App.js
@@ -1,3 +1,12 @@
-const heading = React.createElement("h1", {id : "heading"}, "Hello World From React!!");
+const parent = React.createElement("div", {id : "parent"}, 
+React.createElement("div", {id : "child"}, 
+[React.createElement("h1", {id: "h1"}, "I am an h1 tag!!"),
+ React.createElement("h2", {id: "h2"}, "I am an h2 tag!!"), 
+ React.createElement("h3", {id: "h3"}, "I am an h3 tag!!")]
+));
+
+console.log(parent);
+
+//const heading = React.createElement("h1", {id : "heading"}, "Hello World From React!!");
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(parent);

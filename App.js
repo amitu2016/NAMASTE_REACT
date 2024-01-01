9f4b114a1bb5b8ed7cfd4fc008d79f2db75f53b5
Amit Upadhyay <amitu@cdac.in>
2024-01-02 00:31:50 +0530

    commit

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child1" }, [
     React.createElement("h1", { id: "h1" }, "I am an h1 tag!!"),

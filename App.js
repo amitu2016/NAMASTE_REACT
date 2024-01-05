15985ee37e0f7caebc5b56c048a5b71294ec116d
Amit Upadhyay <amitu@cdac.in>
2024-01-05 07:35:01 +0530

    episode 3

--- App.js
+++ App.js
@@ -1,20 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", { id: "h1" }, "This is Namaste React"),
-    React.createElement("h2", { id: "h2" }, "I am an h2 tag!!"),
-    React.createElement("h3", { id: "h3" }, "I am an h3 tag!!"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", { id: "h11" }, "I am an h1 tag!!"),
-    React.createElement("h2", { id: "h21" }, "I am an h2 tag!!"),
-    React.createElement("h3", { id: "h31" }, "I am an h3 tag!!"),
-  ]),
-]);
 
-console.log(parent);
+//React.createElement => Object => HTMLElement(render)
+
+//const heading = React.createElement("h1", {id: "heading"}, "Namaste React 🚀");
+
+//JSX 
+const jsxHeading = <h1 id="heading">Namaste React 🚀 using JSX</h1> 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+
+root.render(jsxHeading);

9aa19d70d012104149667c46670f2d2c9a01c0de
Amit Upadhyay <amitu@cdac.in>
2024-01-07 00:55:39 +0530

    commit episode 2

--- App.js
+++ App.js
@@ -6,9 +6,31 @@ import ReactDOM from "react-dom/client";
 
 //const heading = React.createElement("h1", {id: "heading"}, "Namaste React 🚀");
 
-//JSX 
-const jsxHeading = <h1 id="heading">Namaste React 🚀 using JSX</h1> 
+//React Component
+const TitleComponent = () => (
+    <div className="head" tabIndex="5">
+        <h1 id="heading">Namaste React 🚀 using JSX</h1>
+    </div>
+    );
+
+    //React Element
+    const elem = <span>React Element</span>
+
+    const title = (
+        <div className="head" tabIndex="5">   
+            <h1 id="heading">{elem} Namaste React 🚀 using JSX</h1>
+        </div>
+        );
+
+const HeadingComponent = () => (
+    <div id="container">
+        <TitleComponent />
+        {title}
+        <h1>Namaste React Functional Component</h1>
+    </div>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(jsxHeading);
+root.render(<HeadingComponent />);
+

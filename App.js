f39ea6529fa22d36f66ac9b6b4af332b08bb8318
Amit Upadhyay <amitu@cdac.in>
2024-01-07 12:40:46 +0530

    header component

--- App.js
+++ App.js
@@ -1,36 +1,47 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import logo from './logo.png';
 
-
-//React.createElement => Object => HTMLElement(render)
-
-//const heading = React.createElement("h1", {id: "heading"}, "Namaste React 🚀");
-
-//React Component
-const TitleComponent = () => (
-    <div className="head" tabIndex="5">
-        <h1 id="heading">Namaste React 🚀 using JSX</h1>
-    </div>
-    );
-
-    //React Element
-    const elem = <span>React Element</span>
-
-    const title = (
-        <div className="head" tabIndex="5">   
-            <h1 id="heading">{elem} Namaste React 🚀 using JSX</h1>
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="logo-container">
+                <img className="logo" src={logo} alt="logo"></img>
+            </div>
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Contact Us</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
         </div>
-        );
+    );
+}
 
-const HeadingComponent = () => (
-    <div id="container">
-        <TitleComponent />
-        {title}
-        <h1>Namaste React Functional Component</h1>
+const Body = () => {
+    return (
+        <div className="body">
+            <div className="search">
+                Search
+            </div>
+            <div className="res-container">
+
+            </div>
+        </div>
+    );
+}
+
+const AppLayout = () => {
+    return (
+    <div className="app">
+        <Header />
     </div>
-);
+    )
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<HeadingComponent />);
+root.render(<AppLayout />);
 

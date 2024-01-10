2d78549bf28b5dce3e38a54466dc0c3482d208e5
Amit Upadhyay <amitu@cdac.in>
2024-01-10 16:34:11 +0530

    Login Button Added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,12 @@
+import { useState } from 'react';
 import logo from '../utils/logo.png';
 
 const Header = () => {
+
+    //Powerful local variable : useState
+    //Whenever the btnName will be changed using setBtnName, React will re render the whole component 
+    const [btnName, setBtnName] = useState("Login");
+
     return (
         <div className="header">
             <div className="logo-container">
@@ -12,6 +18,9 @@ const Header = () => {
                     <li>About Us</li>
                     <li>Contact Us</li>
                     <li>Cart</li>
+                    <button className='login' onClick={() => {
+                       btnName === "Login" ? setBtnName("Logout") : setBtnName("Login")
+                    }}>{btnName}</button>
                 </ul>
             </div>
         </div>

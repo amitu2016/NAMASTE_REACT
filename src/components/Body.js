27a84a1bf9a2de5d064bd2878a0cd53c548658ba
Amit Upadhyay <amitu@cdac.in>
2024-01-10 14:23:20 +0530

    Episode 6 Exploring World

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,13 +1,33 @@
 import RestaurentCard from "./RestaurentCard";
-import resList from "../utils/mockdata";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import Shimmer from "./Shimmer";
 
 
 const Body = () => {
 
     //Local state variable -> powerful js variable -> useState hook
-    const [listOfRestaurents, setListOfRestaurents] = useState(resList);
+    const [listOfRestaurents, setListOfRestaurents] = useState([]);
 
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+
+    const fetchData = async () => {
+        const data = await fetch(
+            "https://swiggy-api-bypass-cors.vercel.app/api/restaurants?lat=19.025330676643726&lng=73.0548236544475"
+            );
+
+            const json = await data.json();
+
+            //console.log(json.data.cards[5].card.card.gridElements.infoWithStyle.restaurants)
+
+            setListOfRestaurents(json?.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
+    }
+
+    if (listOfRestaurents.length === 0) {
+        return <Shimmer />
+    }
 
     return (
         <div className="body">

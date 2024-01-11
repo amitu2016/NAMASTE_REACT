0df86a24142f6f648b9fdfafb54318a5d0d34915
Amit Upadhyay <amitu@cdac.in>
2024-01-11 15:34:53 +0530

    Added Link from router

--- src/App.js
+++ src/App.js
@@ -5,14 +5,14 @@ import Body from "./components/Body";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Error from "./components/Error";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 
 
 const AppLayout = () => {
     return (
     <div className="app">
         <Header />
-        <Body />
+        <Outlet />
     </div>
     )
 }
@@ -21,15 +21,21 @@ const appRouter = createBrowserRouter([
     {
         path: "/",
         element: <AppLayout />,
+        children: [
+            {
+                path: "/",
+                element: <Body />
+            },
+            {
+                path: "/about",
+                element: <About />
+            },
+            {
+                path: "/contact",
+                element: <Contact />
+            }
+        ],
         errorElement: <Error />
-    },
-    {
-        path: "/about",
-        element: <About />
-    },
-    {
-        path: "/contact",
-        element: <Contact />
     }
 ]);
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import logo from '../utils/logo.png';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
 
@@ -21,10 +22,10 @@ const Header = () => {
             </div>
             <div className="nav-items">
                 <ul>
-                    <li>Home</li>
-                    <li>About Us</li>
-                    <li>Contact Us</li>
-                    <li>Cart</li>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/about">About Us</Link></li>
+                    <li><Link to="/contact">Contact Us</Link></li>
+                    <li><Link>Cart</Link></li>
                     <button className='login' onClick={() => {
                        btnName === "Login" ? setBtnName("Logout") : setBtnName("Login")
                     }}>{btnName}</button>
